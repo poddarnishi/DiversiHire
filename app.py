42e66e7124579e22df1d98c2c5c1443bd97cd495
vaishnavirshah <vaishnavirahulshah@gmail.com>
2024-02-18 01:03:08 -0500

    fixed formatting for resume

--- app.py
+++ app.py
@@ -222,4 +222,4 @@ def display_pdf_text(pdf_text):
     return render_template('display.html', pdf_text=pdf_text)
   
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.1')
+    app.run(debug=True)
